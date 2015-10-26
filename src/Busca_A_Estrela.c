f3903b05babd24b9aaf73fa21095601f401f6364
Rene Alves <renealvesbarbosa@gmail.com>
2015-10-26 13:14:30 -0300

    Adicionado a leitura de arquivo

--- src/Busca_A_Estrela.c
+++ src/Busca_A_Estrela.c
@@ -16,11 +16,21 @@
 
  int main(int argc, char** argv){
  	char * nome_arquivo;
+ 	FILE * arquivo;
 
  	if(argc != 2){
  		erroArgs();
  		return 0;
  	}
  	
+ 	nome_arquivo = argv[1];
+
+ 	arquivo = fopen(nome_arquivo, "r");
+
+ 	if(arquivo == NULL){
+ 		printf("Arquivo não encontrado, por favor, forceça a tabela de entrada do algoritimo!\n");
+ 		return -1;
+ 	}
+
  	return 1; 	
  }
